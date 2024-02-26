95a758e017125760c30bbcb37dc70482118e6a37
Ned Palacios <npdoesmc@gmail.com>
2024-02-26 20:59:28 +0800

    fix: use faq accordion

--- home.php
+++ home.php
@@ -526,15 +526,15 @@ get_header();
                 <div class="faqs flex flex-col">
                     <?php foreach ($faq as $idx => $faqItem) { ?>
                         <div class="faq-item border-x border-b border-white/20 bg-white/10 transition-colors hover:bg-white/30 <?php echo $idx == 0 ? "border-t rounded-t-lg" : ( $idx == count( $faq ) - 1 ? " rounded-b-lg" : "" ) ?>">
-                            <div class="faq-question transition-transform cursor-pointer py-6 px-8 flex items-start">
-                                <p class="font-bold flex-1"><?php echo $faqItem['question'] ?></p>
+                            <div class="faq-question transition-transform cursor-pointer px-8 flex items-start">
+                                <p class="font-bold py-6 flex-1"><?php echo $faqItem['question'] ?></p>
 
-                                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M20 12.5L32.5 25L30.75 26.75L20 16L9.25 26.75L7.5 25L20 12.5Z" fill="white"/>
+                                <svg class="mt-5" xmlns="http://www.w3.org/2000/svg" fill="none" width="40" height="40" viewBox="0 0 40 40">
+                                    <path fill="#fff" d="M20 12.5 32.5 25l-1.75 1.75L20 16 9.25 26.75 7.5 25 20 12.5Z"/>
                                 </svg>
                             </div>
 
-                            <div class="faq-answer mt-0 mb-6 pl-3 pr-8 ml-8 border-l border-[#A8E54E]">
+                            <div class="faq-answer mt-0 pl-3 pr-8 ml-8 mb-6 border-l border-[#A8E54E]">
                                 <p class="text-sm"><?php echo $faqItem['answer'] ?></p>
                             </div>
                         </div>
