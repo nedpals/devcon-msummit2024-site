c45a5803a64fcd82e1b21de919921657d6f02fe7
Ned Palacios <npdoesmc@gmail.com>
2024-03-22 14:39:43 +0800

    fix: make social links a template part, fix footer menu

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -8,19 +8,23 @@
 ?>
 
 <footer class="relative text-white h-24 pl-4 lg:pl-0">
-    <div class="container !px-0 py-8 flex flex-col lg:flex-row items-center space-y-4 lg:space-y-0 lg:space-x-12 border-t border-white/20">
+    <div class="container w-full !px-0 py-8 flex flex-col lg:flex-row items-center border-t border-white/20">
         <img src="<?php devcon_msummit2024_get_asset_url('devcon_logo.png') ?>" />
 
-        <div class="flex flex-col md:flex-row items-center font-light text-gray-300">
-            <p class="lg:pr-6">&copy; 2024 DEVCON</p>
+        <div class="flex mt-8 lg:my-0 lg:mx-auto flex-col-reverse md:flex-row items-center font-light text-gray-300">
+            <p>&copy; 2024 DEVCON</p>
 
-            <ul class="list-none m-0 flex items-center space-x-6">
-                <?php wp_nav_menu([
-                    'menu' => 'footer',
-                    'container' => false,
-                    'items_wrap' => '%3$s',
-                ]) ?>
-            </ul>
+            <div class="hidden lg:block h-6 w-[1px] mx-8 bg-gray-300"></div>
+
+	        <?php get_template_part('template-parts/navigation/social-links', args: ['class' => 'lg:hidden mb-4']); ?>
+
+	        <?php wp_nav_menu([
+		        'menu' => 'footer',
+		        'menu_class' => 'footer-menu my-8',
+                'container_class' => 'mb-4 md:mb-0'
+	        ]) ?>
         </div>
+
+	    <?php get_template_part('template-parts/navigation/social-links', args: ['class' => 'hidden lg:flex']); ?>
     </div>
 </footer>
