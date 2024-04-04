c232397d796352ca41f97d54f0fa30f59deed95b
Ned Palacios <npdoesmc@gmail.com>
2024-04-04 09:01:12 +0800

    feat: improve packaging mechanism. include auto-update from githu

---
+++ inc/auto-updater.php
@@ -0,0 +1,36 @@
+<?php
+// Automatic updates from GitHub
+// Modified and taken from: https://gist.github.com/slfrsn/a75b2b9ef7074e22ce3b
+
+function automatic_gh_updates($data) {
+	// Theme information
+	$theme   = get_stylesheet(); // Folder name of the current theme
+	$current = wp_get_theme()->get('Version'); // Get the version of the current theme
+
+	// GitHub information
+	$user = '{{GH-USERNAME}}'; // The GitHub username hosting the repository
+	$repo = '{{GH-REPO-NAME}}'; // Repository name as it appears in the URL
+
+	// Get the latest release tag from the repository. The User-Agent header must be sent, as per
+	// GitHub's API documentation: https://developer.github.com/v3/#user-agent-required
+	$file = @json_decode(@file_get_contents('https://api.github.com/repos/'.$user.'/'.$repo.'/releases/latest', false,
+		stream_context_create(['http' => ['header' => "User-Agent: ".$user."\r\n"]])
+	));
+
+	if($file) {
+		// Strip the version number of any non-numerical characters
+		$update = preg_replace('/[^0-9.].*/', '', $file->tag_name);
+		// Only return a response if the new version number is higher than the current version
+		if($update > $current) {
+			$data->response[$theme] = array(
+				'theme'       => $theme,
+				'new_version' => $update,
+				'url'         => 'https://github.com/'.$user.'/'.$repo,
+				'package'     => $file->assets[0]->browser_download_url,
+			);
+		}
+	}
+	return $data;
+}
+
+add_filter('pre_set_site_transient_update_themes', 'automatic_gh_updates', 100, 1);
--- package.py
+++ package.py
@@ -8,28 +8,44 @@ import os
 
 __dir__ = os.path.dirname(os.path.realpath(__file__))
 
-themeZip = zipfile.ZipFile('theme.zip', 'w')
+# modify inc/auto-updater.php if GH_USERNAME and GH_REPO are defined
+if 'GH_USERNAME' in os.environ and 'GH_REPO' in os.environ:
+    with open(os.path.join(__dir__, 'inc', 'auto-updater.php'), 'rw') as file:
+        data = file.read()
+        data = data.replace('GH_USERNAME', os.environ['GH_USERNAME'])
+        data = data.replace('GH_REPO', os.environ['GH_REPO'])
+        file.write(data)
 
+    # include the auto-updater.php inside functions.php
+    with open(os.path.join(__dir__, 'functions.php'), 'a') as file:
+        file.write("\nrequire get_template_directory() . '/inc/auto-updater.php';\n")
+
+# modify style.css if THEME_VERSION is defined
+if 'THEME_VERSION' in os.environ:
+    with open(os.path.join(__dir__, 'style.css'), 'rw') as file:
+        data = file.read()
+        data = data.replace('Version: 1.0.0', f"Version: {os.environ['THEME_VERSION']}")
+        file.write(data)
+
+# Package the theme
+theme_name = os.path.basename(os.path.normpath(__dir__))
 files_to_exclude = ['.git/**/*', '.github/**/*', 'package.py', 'theme.zip', '.idea/**/*', 'phpcs.xml.dist', 'node_modules/**/*', 'package.json', 'package-lock.json']
 files_to_exclude = list(map(lambda x: os.path.join(__dir__, x.replace("/", os.sep)), files_to_exclude))
-
 files = glob.glob(os.path.join(__dir__, '**', '*'), recursive=True)
-theme_name = os.path.basename(os.path.normpath(__dir__))
 
-for file in files:
-    excluded = False
-    rel = os.path.relpath(file, __dir__)
-    
-    for exclude in files_to_exclude:
-        if fnmatch.fnmatch(file, exclude):
-            excluded = True
-            break
+with zipfile.ZipFile('theme.zip', 'w') as themeZip:
+    for file in files:
+        excluded = False
+        rel = os.path.relpath(file, __dir__)
         
-    if excluded:
-        print('Excluded: ' + rel)
-        continue
-    
-    print('Added: ' + rel)
-    themeZip.write(file, os.path.join(theme_name, rel))
-    
-themeZip.close()
+        for exclude in files_to_exclude:
+            if fnmatch.fnmatch(file, exclude):
+                excluded = True
+                break
+            
+        if excluded:
+            print('Excluded: ' + rel)
+            continue
+        
+        print('Added: ' + rel)
+        themeZip.write(file, os.path.join(theme_name, rel))
