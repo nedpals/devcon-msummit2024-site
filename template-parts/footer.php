cff3846d10e6784a6cc7b5fd6a69fefd3825049e
Ned Palacios <npdoesmc@gmail.com>
2024-04-04 11:17:04 +0800

    fix: footer styling on tablet

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -11,7 +11,7 @@
     <div class="container w-full xl:px-0 py-8 flex flex-col lg:flex-row items-center border-t border-white/20">
         <img src="<?php devcon_msummit2024_get_asset_url('devcon_logo.png') ?>" />
 
-        <div class="flex mt-8 lg:my-0 lg:mx-auto flex-col-reverse md:flex-row items-center font-light text-gray-300">
+        <div class="mt-8 lg:my-0 lg:mx-auto flex flex-col-reverse lg:flex-row items-center font-light text-gray-300">
             <p>&copy; 2024 DEVCON</p>
 
             <div class="hidden lg:block h-6 w-[1px] mx-8 bg-gray-300"></div>
@@ -21,7 +21,7 @@
 	        <?php wp_nav_menu([
 		        'menu' => 'footer',
 		        'menu_class' => 'footer-menu my-8',
-                'container_class' => 'mb-4 md:mb-0'
+                'container_class' => 'mb-4 lg:mb-0'
 	        ]) ?>
         </div>
 
