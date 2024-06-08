84fcba70adfdafe312efd00b4c5cec2d3cc67e42
Ned Palacios <npdoesmc@gmail.com>
2024-06-08 10:34:34 +0800

    fix: speaker styling

--- template-parts/landing-page/speakers.php
+++ template-parts/landing-page/speakers.php
@@ -15,14 +15,13 @@ $speakers = devcon_msummit2024_get_theme_mod('speakers', []);
 
     <div class="relative pt-4">
         <div class="swiper" data-swiper-autoplay="1000">
-            <div class="swiper-wrapper speakers-list flex-1 flex items-center pt-8">
+            <div class="swiper-wrapper speakers-list flex-1 flex items-stretch pt-8">
 		        <?php foreach ($speakers as $speaker) { ?>
-                    <div class="swiper-slide max-w-[24rem] px-2 pb-2 pt-32">
+                    <div class="swiper-slide max-w-[24rem] h-full px-2 pb-2 pt-32">
                         <div class="flex flex-col items-center border border-white/20 bg-white/10 rounded-2xl">
                             <!-- image goes here -->
-                            <div
-                                    class="flex items-center justify-center bg-contain bg-center bg-no-repeat h-96 w-full max-w-[24rem] -mt-32"
-                                    style="background-image: url(<?php devcon_msummit2024_get_asset_url('speaker_bg.png'); ?>)">
+                            <div class="flex items-center justify-center bg-contain bg-center bg-no-repeat h-96 w-full max-w-[24rem] -mt-32"
+                                 style="background-image: url(<?php devcon_msummit2024_get_asset_url('speaker_bg.png'); ?>)">
                                 <img src="<?php echo $speaker['photo'] ?>"
                                      class="h-64 w-auto object-cover"
                                      alt="<?php echo $speaker['name'] ?>">
@@ -30,11 +29,11 @@ $speakers = devcon_msummit2024_get_theme_mod('speakers', []);
 
                             <div class="flex flex-col items-center text-center space-y-12 px-6 pb-10 -mt-12">
                                 <div class="space-y-2">
-                                    <h3 class="uppercase text-2xl tracking-wide font-bold"><?php echo $speaker['name'] ?></h3>
-                                    <p class="uppercase font-semibold"><?php echo $speaker['position'] ?></p>
+                                    <h3 class="uppercase text-3xl tracking-wide font-bold"><?php echo $speaker['name'] ?></h3>
+                                    <p class="uppercase font-medium"><?php echo $speaker['position'] ?></p>
                                 </div>
 
-                                <p class="text-light text-sm"><?php echo $speaker['bio'] ?></p>
+<!--                                <p class="text-light text-sm">--><?php //echo $speaker['bio'] ?><!--</p>-->
                             </div>
                         </div>
                     </div>
